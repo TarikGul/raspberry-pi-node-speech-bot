ab999aff024cf36e512b432493f008d244a708af
Tarik <tariksnow37@gmail.com>
2020-10-05 14:20:31 -0700

    google cloud text to speech

---
+++ speech/text_to_speech.js
@@ -0,0 +1,29 @@
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+async function quickStart() {
+    // The text to synthesize
+    const text = 'hello, world!';
+
+    // Construct the request
+    const request = {
+        input: { text: text },
+        // Select the language and SSML voice gender (optional)
+        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+        // select the type of audio encoding
+        audioConfig: { audioEncoding: 'MP3' },
+    };
+
+    // Performs the text-to-speech request
+    const [response] = await client.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile('output.mp3', response.audioContent, 'binary');
+    console.log('Audio content written to file: output.mp3');
+}
+quickStart();
