65dac4b1153d0a45798daff655c52188a862f0e7
Tarik <tariksnow37@gmail.com>
2020-10-05 16:47:31 -0700

    keep it seperate and clean up code

---
+++ external_api/weather.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const messages = require('../util/messages/weather_messages');
+
+require('dotenv').config({ path: '../.env' });
+
+const weatherURL = (key, cityId) => {
+    return `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${key}`
+};
+
+const weatherData = async (key) => {
+    axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+    const cityId = 1689969; // => San Francisco
+    const url = weatherURL(key, cityId)
+    // await the result if there is an error then console it
+    let res = await axios.get(url).catch(err => {
+        console.log(err);
+    });
+
+    return res.data;
+}
+
+module.exports = {
+    weatherData
+};
+/**
+ * 
+ * I should probably abstract construct message out of here 
+ */
+
+
+
