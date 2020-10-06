bb9ae54ea0e70db2b9711b08aed73fafe3c51644
Tarik <tariksnow37@gmail.com>
2020-10-06 12:29:15 -0700

    fix little mistake

--- main/index.js
+++ main/index.js
@@ -1,6 +1,6 @@
 // This is basically the controller for the raspberry pi
 const { weatherData } = require('./external_api/weather');
-const { constructWeatherMessage } = require('./messaging/weather_messaging');
+const { constructWeatherMessage, WeatherBot,  } = require('./messaging/weather_messaging');
 const writeAudioTextToSpeech = require('../speech/text_to_speech');
 
 const env = require('../config/enviornments');
@@ -12,22 +12,28 @@ const key = env.WEATHER_KEY;
  *  [] 
  */
 
-const weatherBot = () => {
-    weatherData(key)
+const weatherBot = async () => {
+    // I should cache data to minimize calls to API. 
+    // Example, what if I ask what the weather is, and I ask again because I 
+    // Use caching with child proccesses. 
+    let msg;
+    await weatherData(key)
         .then((data) => {
             msg = constructWeatherMessage(data);
-            writeAudioTextToSpeech(msg);
+            // writeAudioTextToSpeech(msg);
             console.log(msg)
         })
         .catch((err) => {
             console.log(err.message)
         });
+
+    // const bot = new WeatherBot(result);
+    // bot.constructCurrentWeatherMessage()
 }
 
 // Make a request to the API
 // The return value will be a string that will be echoed
 if (require.main === module) {
-    let msg;
 
     weatherBot()
 }
