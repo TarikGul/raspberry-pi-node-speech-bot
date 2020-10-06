bbaa78011038f3c8247d7ad57c0d391740b24f5c
Tarik <tariksnow37@gmail.com>
2020-10-05 20:55:22 -0700

    catch error on api call

--- main/index.js
+++ main/index.js
@@ -1,18 +1,23 @@
 // This is basically the controller for the raspberry pi
-const { weatherData } = require('../external_api/weather');
+const { weatherData } = require('./external_api/weather');
 const { constructWeatherMessage } = require('./messaging/weather_messaging')
 
-require('dotenv').config({ path: '../.env' });
+const env = require('../config/enviornments');
 
-const key = process.env.WEATHER_KEY;
+const key = env.WEATHER_KEY;
 
 // Make a request to the API
 // The return value will be a string that will be echoed
 if (require.main === module) {
     let msg;
 
-    weatherData(key).then((data) => {
-        msg = constructWeatherMessage(data);
-        console.log(msg)
-    })
+    weatherData(key)
+        .then((data) => {
+            msg = constructWeatherMessage(data);
+            console.log(msg)
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+
 }
