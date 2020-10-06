95973872d25d2727ea51420f65f3aa31f6f6030e
Tarik <tariksnow37@gmail.com>
2020-10-06 00:43:25 -0700

    start abstracting the bot

--- main/index.js
+++ main/index.js
@@ -1,23 +1,33 @@
 // This is basically the controller for the raspberry pi
 const { weatherData } = require('./external_api/weather');
-const { constructWeatherMessage } = require('./messaging/weather_messaging')
+const { constructWeatherMessage } = require('./messaging/weather_messaging');
+const writeAudioTextToSpeech = require('../speech/text_to_speech');
 
 const env = require('../config/enviornments');
 
 const key = env.WEATHER_KEY;
+
+/**
+ * @TODO 
+ *  [] 
+ */
+
+const weatherBot = () => {
+    weatherData(key)
+        .then((data) => {
+            msg = constructWeatherMessage(data);
+            writeAudioTextToSpeech(msg);
+            console.log(msg)
+        })
+        .catch((err) => {
+            console.log(err.message)
+        });
+}
 
 // Make a request to the API
 // The return value will be a string that will be echoed
 if (require.main === module) {
     let msg;
 
-    weatherData(key)
-        .then((data) => {
-            msg = constructWeatherMessage(data);
-            console.log(msg)
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
-
+    weatherBot()
 }
