34b5d05ca768566b503c5f7e75c7c3288239fee3
Tarik <tariksnow37@gmail.com>
2020-10-06 23:50:47 -0700

    add more configs for db

--- config/enviornments.js
+++ config/enviornments.js
@@ -4,5 +4,8 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 module.exports = {
     PORT: process.env.PORT,
     WEATHER_KEY: process.env.WEATHER_KEY,
-    USER_NAME: process.env.USER_NAME
+    USER_NAME: process.env.USER_NAME,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER_NAME: process.env.DB_USER_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD
 }
