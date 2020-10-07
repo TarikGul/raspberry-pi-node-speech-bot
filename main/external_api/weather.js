8259cf25e30a30ed4578a4654e925c47e88ea8b2
Tarik <tariksnow37@gmail.com>
2020-10-07 01:29:05 -0700

    change the way we pass in cityId

--- main/external_api/weather.js
+++ main/external_api/weather.js
@@ -4,11 +4,10 @@ const weatherURL = (key, cityId) => {
     return `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${key}&units=imperial`
 };
 
-const weatherData = async (key) => {
+const weatherData = async (key, cityId = 5391959) => { // => San Francisco ID with openweatherAPI
     axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
     axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-
-    const cityId = 5391959; // => San Francisco ID with openweatherAPI
+    
     const url = weatherURL(key, cityId);
     const errors = {};
     // await the result if there is an error then console it
