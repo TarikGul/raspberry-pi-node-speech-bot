c496169194c8829e10f1f7e01f1b147d337e6658
Tarik <tariksnow37@gmail.com>
2020-10-10 19:19:41 -0700

    use execute js

--- audio_processing/binding_scripts.py
+++ audio_processing/binding_scripts.py
@@ -1,8 +1,21 @@
 import os
+import concurrent.futures
+from multiprocessing.pool import ThreadPool
+import threading
+from Naked.toolshed.shell import execute_js, muterun_js
+
+
 
 def run_cora():
-    # need to create proper flags to run the node operation correctly
-    os.system('node ../index.js');
+    response = execute_js('../lib/index.js')
+    return response
 
+def stop_cora():
+    print('stopped cora')
+
+# Cant use the Virtual env in production when using Google Cloud
+# Cant acces global env variables.
+def setupVirtualEnv():
+    os.system('source env/bin/activate')
 
 
