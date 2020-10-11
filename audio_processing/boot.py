ef17746cc8c473df74a3dde13fb61f8051265b27
Tarik <tariksnow37@gmail.com>
2020-10-10 19:25:57 -0700

    boot

---
+++ audio_processing/boot.py
@@ -0,0 +1,12 @@
+from live_background_recognition import live_background_recognition
+from binding_scripts import run_cora, setupVirtualEnv
+
+def boot_recognition():
+    live_background_recognition()
+
+if __name__ == "__main__":
+    boot_recognition()
+
+boot_recognition()
+# print('hi')
+
