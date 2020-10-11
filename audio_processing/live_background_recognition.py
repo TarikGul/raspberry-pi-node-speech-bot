c3be4af4718198c22e56b5d0132c94b71de1c2d6
Tarik <tariksnow37@gmail.com>
2020-10-10 19:20:34 -0700

    make adjustment to live background. Going to use threaded_worker instead

--- audio_processing/live_background_recognition.py
+++ audio_processing/live_background_recognition.py
@@ -1,13 +1,15 @@
 import time
+import threading
+import sys
+import queue
 import speech_recognition as sr
 from binding_scripts import run_cora
 
-# this is called from the background thread
 
 def boot_countdown():
     print('There is a 5 second buffer before Kora starts listening')
     i = 5
-    for _ in range(5):
+    for _ in range(2):
         print(i)
         i = i - 1
         time.sleep(1)
@@ -18,9 +20,11 @@ def callback(recognizer, audio):
     # received audio data, now we'll recognize it using Google Speech Recognition
     try:
         text = recognizer.recognize_sphinx(audio)
-        if text == 'hello cora':
-            run_cora()
-            return
+        if text == 'hello' or text == 'oh':
+            print("You successfully said hello or oh: -> " + text)
+            run_bot_result = run_cora()
+            
+            # sys.exit()    
         print("CMU Sphinx Recognition thinks you said " + text)
     except sr.UnknownValueError:
         print("CMU Sphinx Recognition could not understand audio")
@@ -34,6 +38,9 @@ def live_background_recognition():
     r = sr.Recognizer()
     m = sr.Microphone()
 
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+
     stop_listening = r.listen_in_background(m, callback)
 
     stop_listening()
@@ -42,9 +49,11 @@ def live_background_recognition():
 
     stop_listening = r.listen_in_background(m, callback)
 
-    while True:
+    while threading.active_count() > 1:
         # do other things on the main thread!
         time.sleep(0.1)
+
+    return
 
 # To call speech_recognition from Command Line 
 # $python -m speech_recognition
