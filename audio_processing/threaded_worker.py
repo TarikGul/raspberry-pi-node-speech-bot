4f77be64d5e9ef67805b54f1ddb795d906fbe7dc
Tarik <tariksnow37@gmail.com>
2020-10-15 23:02:06 -0700

    add better caching for hello response

--- audio_processing/threaded_worker.py
+++ audio_processing/threaded_worker.py
@@ -9,7 +9,7 @@ except ImportError:
     from Queue import Queue  # Python 2 import
 
 import speech_recognition as sr
-from binding_scripts import run_cora
+from binding_scripts import run_cora, run_greeting_audio
 from cachetools import TTLCache
 
 r = sr.Recognizer()
@@ -17,8 +17,7 @@ audio_queue = Queue()
 
 # Step 1:
 # Setup a cache to store when we start with a hello, and give it a 6 second TTL
-hello_cache = TTLCache(10, 6, timer=time.monotonic, getsizeof=None)
-response_cache = TTLCache(10, 10, timer=time.monotonic, getsizeof=None)
+
 # Step 2:
 # Do Aho Corasick Word matching. only if the first cache is a 'hello';
 
@@ -29,14 +28,18 @@ response_cache = TTLCache(10, 10, timer=time.monotonic, getsizeof=None)
 # This could be as simple as writing an audio file with my name, and giving a message back
 # But if you want to you can make a node bot for it too that will give a custom message back
 
-
+# This runs in a background thread
 def recognize_worker():
-    # this runs in a background thread
+    # Cache the hello response on for <6 seconds>
+    hello_cache = TTLCache(10, 6, timer=time.monotonic, getsizeof=None)
+    # Cache the response time for asking a question <10 seconds>
+    response_cache = TTLCache(10, 10, timer=time.monotonic, getsizeof=None)
+
     while True:
         audio = audio_queue.get()  # retrieve the next audio processing job from the main thread
         if audio is None:
-            print('sleeping for 5 seconds to wait for response');
-            time.sleep(5)
+            print('[SLEEPING] microphone is sleeping for 2 seconds');
+            time.sleep(2)
             continue
             # break can stop processing if the main thread is done
         # received audio data, now we'll recognize it using CMUSphinx Recognition
@@ -44,11 +47,38 @@ def recognize_worker():
             text = r.recognize_sphinx(audio)
             print("Google Speech Recognition thinks you said " + text)
 
-            # This will be our decision Tree
+            """
+            Section: WORD RANKING
+            This is the word ranking section of the thread. It will return a string
+            that will denote what the bot should do. 
 
-            if text == 'hello':
-                print('Kora recognized your hello')
+            NOTE I am not sure if I should also run this on another thread, and not
+            the same one as the audio_processing
+            """
+
+            # This will be our decision Tree
+            #
+            # This will basically decide if we are going to do the hello bot
+            # or weather bot. We will implement a search bot soon.
+
+            """
+            Section: HELLO CORA
+            This is the 'HELLO CORA' portion of the bot, and interacts with:
+            -> GreetingBot
+            """
+            is_hello_cached = hello_cache.currsize
+            if(is_hello_cached == 1 and text == 'hello'):
+                # Run the 'You just said hello whats up?' message
+                run_greeting_audio('-cheeky')
+                continue
+            elif is_hello_cached == 0 and text == 'hello':
+                print('[RECOGNIZED]: Kora recognized your hello')
+
+                # Set the TTLCache to a 6 second timer
+                hello_cache.__setitem__('hello', True)
+
                 response = run_cora()
+                run_greeting_audio()
                 if response is True:
                     audio_queue.put(None)
                     continue
@@ -70,6 +100,7 @@ def recognize_worker():
 recognize_thread = Thread(target=recognize_worker)
 recognize_thread.daemon = True
 recognize_thread.start()
+
 with sr.Microphone() as source:
     try:
         while True:  # repeatedly listen for phrases and put the resulting audio on the audio processing job queue
@@ -79,6 +110,7 @@ with sr.Microphone() as source:
         print('hit false')
     except KeyboardInterrupt:  # allow Ctrl + C to shut down the program
         pass
+
 audio_queue.join()  # block until all current audio processing jobs are done
 audio_queue.put(None)  # tell the recognize_thread to stop
 recognize_thread.join()  # wait for the recognize_thread to actually stop
