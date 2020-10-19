9797bf97f619f7009dbb281fe527dbddd7d69860
Tarik <tariksnow37@gmail.com>
2020-10-18 20:01:28 -0700

    add more concurrency logic

--- audio_processing/threaded_worker.py
+++ audio_processing/threaded_worker.py
@@ -30,22 +30,42 @@ audio_queue = Queue()
 
 # This runs in a background thread
 def recognize_worker():
-    # Cache the hello response on for <6 seconds>
-    hello_cache = TTLCache(10, 6, timer=time.monotonic, getsizeof=None)
+    # Cache the hello response on for <10 seconds>
+    hello_cache = TTLCache(10, 60, timer=time.monotonic, getsizeof=None)
     # Cache the response time for asking a question <10 seconds>
     response_cache = TTLCache(10, 10, timer=time.monotonic, getsizeof=None)
+    # Cache the response time so the mic does use computing power on speech when cora is speaking
+    cora_responding_cache = TTLCache(10, 3, timer=time.monotonic, getsizeof=None)
 
     while True:
         audio = audio_queue.get()  # retrieve the next audio processing job from the main thread
+
+        if isinstance(audio, str):
+            # We pass a string into audio_queue and if we receive a string it means to 
+            # print that action
+            audio_queue.task_done()
+            print('[REFRESH]: {} the MIC'.format(audio))
+            continue
+
         if audio is None:
+            
             print('[SLEEPING] microphone is sleeping for 2 seconds');
-            time.sleep(2)
+            # time.sleep(2)
             continue
             # break can stop processing if the main thread is done
         # received audio data, now we'll recognize it using CMUSphinx Recognition
         try:
+            #Make sure to not listen when cora is talking
+            print('[SIZE CACHE]: ', cora_responding_cache.currsize)
+            if cora_responding_cache.currsize > 0:
+                print('--------------------hit-response-cache-------------------')
+                # cora_responding_cache.popitem()
+                audio_queue.task_done()
+                # time.sleep(0.1)
+                continue
+
             text = r.recognize_sphinx(audio)
-            print("Google Speech Recognition thinks you said " + text)
+            print("[TRANSCRIBED_AUDIO]: " + text)
 
             """
             Section: WORD RANKING
@@ -69,7 +89,9 @@ def recognize_worker():
             is_hello_cached = hello_cache.currsize
             if(is_hello_cached == 1 and text == 'hello'):
                 # Run the 'You just said hello whats up?' message
+                print('---------------------hit the cache----------------------')
                 run_greeting_audio('-cheeky')
+                audio_queue.task_done()
                 continue
             elif is_hello_cached == 0 and text == 'hello':
                 print('[RECOGNIZED]: Kora recognized your hello')
@@ -78,21 +100,21 @@ def recognize_worker():
                 hello_cache.__setitem__('hello', True)
 
                 response = run_cora()
-                run_greeting_audio()
+                audio_queue.put('refresh')
                 if response is True:
-                    audio_queue.put(None)
+                    cora_responding_cache.__setitem__('cora', True)
+                    run_greeting_audio()
+                    audio_queue.task_done()
                     continue
 
-            print('You actually said: ', text)
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
+            print("[ERROR] CMUSphinx Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print(
-                "Could not request results from Google Speech Recognition service; {0}".format(e))
+                "Could not request results from CMUSphinx Speech Recognition service; {0}".format(e))
 
         audio_queue.task_done()  # mark the audio processing job as completed in the queue
 
-    print('hit the werd part of the function')
     return False
 
 
@@ -102,12 +124,17 @@ recognize_thread.daemon = True
 recognize_thread.start()
 
 with sr.Microphone() as source:
+    # Only need to adjust the mic once
+    r.adjust_for_ambient_noise(source)
+
     try:
+        print('[MAIN_THREAD]: -------')
         while True:  # repeatedly listen for phrases and put the resulting audio on the audio processing job queue
-            print(recognize_thread)
+            print('[THREAD] ', recognize_thread)
+            # stop_listening = r.listen(source)
             audio_queue.put(r.listen(source))
-            print('audio queue', audio_queue)
-        print('hit false')
+            # audio_queue.put(stop_listening)
+            print('[AUDIO_QUEUE] audio queue', audio_queue)
     except KeyboardInterrupt:  # allow Ctrl + C to shut down the program
         pass
 
