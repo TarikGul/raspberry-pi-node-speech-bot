f2b0461c27a2734f206315123c7ed4a04f01cc29
Tarik <tariksnow37@gmail.com>
2020-10-18 19:58:53 -0700

    move these files

--- audio_processing/binding_scripts.py
+++ audio_processing/binding_scripts.py
@@ -1,10 +1,27 @@
 import os
 import threading
 from multiprocessing.pool import ThreadPool
-from Naked.toolshed.shell import execute_js, muterun_js
+from Naked.toolshed.shell import execute_js, muterun_js, execute
 
 def run_cora():
     response = execute_js('../lib/index.js', arguments='-b g')
+    return response
+
+def run_weather_cora():
+    response = execute_js('../lib/index.js', arguments='-b w')
+    return response
+
+"""
+Section: Running Audio
+MACOSX: Use afplay for testing and running locally.
+RASPBIAN: Use omxplayer, or pyaudio script.
+"""
+def run_weather_audio():
+    response = execute('afplay ../audio_files/weather.mp3')
+    return response
+
+def run_greeting_audio(optionType=''):
+    response = execute('afplay ../audio_files/greeting{}.mp3'.format(optionType))
     return response
 
 def stop_cora():
