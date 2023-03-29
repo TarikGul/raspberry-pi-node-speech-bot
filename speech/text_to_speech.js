911aefdf245d001638520ff9fd2dd997c0e07e5a
tarikgul <tarik@parity.io>
2023-03-29 16:29:59 -0400

    Remove more JS bloat

--- speech/text_to_speech.js
+++ speech/text_to_speech.js
@@ -1,25 +1,17 @@
-// Imports the Google Cloud client library
 const textToSpeech = require('@google-cloud/text-to-speech');
-// Import other required libraries
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-// Creates a client
 const client = new textToSpeech.TextToSpeechClient();
 
 const writeAudioTextToSpeech = async (text, fileTypeName) => {
-    // Construct the request
     const request = {
         input: { text: text },
-        // Select the language and SSML voice gender (optional)
         voice: { languageCode: 'en-GB', ssmlGender: 'FEMALE', voiceName: 'en-GB-Wavenet-C' },
-        // select the type of audio encoding
         audioConfig: { audioEncoding: 'MP3' },
     };
 
-    // Performs the text-to-speech request
     const [response] = await client.synthesizeSpeech(request);
-    // Write the binary audio content to a local file
     const writeFile = util.promisify(fs.writeFile);
     await writeFile(path.join(__dirname, `../audio_files/${fileTypeName}.mp3`), response.audioContent, 'binary');
     console.log('Audio content written to file: output.mp3');
